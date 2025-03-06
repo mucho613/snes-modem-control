2bf7ea304bd488e319213bc72cae5f089a7bec5e
Daisuke Yamamura <mucho613@gmail.com>
2025-03-06 21:43:11 +0900

    fix: tilemap generator

--- tools/tilemap-generator/src/main.rs
+++ tools/tilemap-generator/src/main.rs
@@ -5,7 +5,9 @@ fn main() -> Result<(), std::io::Error> {
     let mut file = File::create("tilemap.bin")?;
 
     let mut data: Vec<u8> = Vec::new();
-    let mut address: u16 = 0;
+    let mut address: u16;
+
+    address = 0x0000;
     for i in 0..0x0260 {
         // 左右の空白は 0x0000 のタイルを指定
         if i % 32 <= 2 || i % 32 >= 29 {
@@ -19,7 +21,33 @@ fn main() -> Result<(), std::io::Error> {
     }
 
     address = 0x00dc;
+    for i in 0..0x01e0 {
+        // 左右の空白は 0x0000 のタイルを指定
+        if i % 32 <= 2 || i % 32 >= 29 {
+            data.push(0);
+            data.push(0);
+            continue;
+        }
+        data.push((address & 0xFF) as u8);
+        data.push(((address >> 8) & 0b0000_0011) as u8);
+        address += 2;
+    }
+
+    address = 0x0000;
     for i in 0..0x0260 {
+        // 左右の空白は 0x0000 のタイルを指定
+        if i % 32 <= 2 || i % 32 >= 29 {
+            data.push(0);
+            data.push(0);
+            continue;
+        }
+        data.push((address & 0xFF) as u8);
+        data.push(((address >> 8) & 0b0000_0011) as u8);
+        address += 2;
+    }
+
+    address = 0x00dc;
+    for i in 0..0x01e0 {
         // 左右の空白は 0x0000 のタイルを指定
         if i % 32 <= 2 || i % 32 >= 29 {
             data.push(0);
